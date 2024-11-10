cf6257f16013b70efb92f7d50715fd4a2987c089
Uzo-Felix <uzochukwuonyekwelu03@gmail.com>
2024-11-10 13:46:42 +0300

    chore: pwa support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
+import withPWA from 'next-pwa';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development'
+  },
+  images: {
+    domains: ['image-domain.com'],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb'
+    },
+  }
 };
 
-export default nextConfig;
+export default withPWA({
+  ...nextConfig,
+  dest: 'public'
+} as const);
