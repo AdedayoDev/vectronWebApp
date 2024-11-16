9e84200847231c8e2767fe2b100846d80b1448ec
marocz <mario.maro111@gmail.com>
2024-11-16 16:33:26 +0100

    removing test

--- jest.config.ts
+++ jest.config.ts
@@ -1,43 +1,59 @@
+// import type { Config } from 'jest';
+// import nextJest from 'next/jest';
+
+// const createJestConfig = nextJest({
+//   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+//   dir: './',
+// });
+
+// // Add any custom config to be passed to Jest
+// const config: Config = {
+//   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+//   testEnvironment: 'jest-environment-jsdom',
+//   moduleNameMapper: {
+//     '^@/(.*)$': '<rootDir>/src/$1',
+//     '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+//   },
+//   collectCoverageFrom: [
+//     'src/**/*.{js,jsx,ts,tsx}',
+//     '!src/**/*.d.ts',
+//     '!src/**/*.stories.{js,jsx,ts,tsx}',
+//     '!src/**/*.test.{js,jsx,ts,tsx}',
+//     '!src/pages/_*.{js,jsx,ts,tsx}'
+//   ],
+//   coverageThreshold: {
+//     global: {
+//       branches: 80,
+//       functions: 80,
+//       lines: 80,
+//       statements: 80
+//     }
+//   },
+//   testPathIgnorePatterns: [
+//     '<rootDir>/.next/',
+//     '<rootDir>/node_modules/',
+//     '<rootDir>/cypress/'
+//   ],
+//   transform: {
+//     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+//   }
+// };
+
+// // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+// export default createJestConfig(config);
+
 import type { Config } from 'jest';
 import nextJest from 'next/jest';
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
 });
 
-// Add any custom config to be passed to Jest
 const config: Config = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
-  },
-  collectCoverageFrom: [
-    'src/**/*.{js,jsx,ts,tsx}',
-    '!src/**/*.d.ts',
-    '!src/**/*.stories.{js,jsx,ts,tsx}',
-    '!src/**/*.test.{js,jsx,ts,tsx}',
-    '!src/pages/_*.{js,jsx,ts,tsx}'
-  ],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80
-    }
-  },
-  testPathIgnorePatterns: [
-    '<rootDir>/.next/',
-    '<rootDir>/node_modules/',
-    '<rootDir>/cypress/'
-  ],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
   }
 };
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 export default createJestConfig(config);
