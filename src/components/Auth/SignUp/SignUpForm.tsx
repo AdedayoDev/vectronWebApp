71681279eddaf7b37b820d65fdf5be66100dffe7
marocz <mario.maro111@gmail.com>
2024-12-21 10:19:43 +0100

    updating apis

--- src/components/Auth/SignUp/SignUpForm.tsx
+++ src/components/Auth/SignUp/SignUpForm.tsx
@@ -22,8 +22,8 @@ const SignUpSchema = z.object({
  email: z.string().email({
     message: "Please enter a valid email address"
 }),
-name: z.string().min(1, {
-    message: "Please enter your name"
+username: z.string().min(1, {
+    message: "Please enter your username"
 }),
 password: z.string().min(8, {
     message: "Password must be at least 8 characters long"
