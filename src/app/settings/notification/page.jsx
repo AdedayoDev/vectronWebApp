a323e455f19dbc168b6e1e0f63e959a77ee5466d
Woruka Onward Onyekachi <worukaonward@gmail.com>
2024-12-25 20:00:04 +0100

    fiexed the cloudinary error

--- src/app/settings/notification/page.jsx
+++ src/app/settings/notification/page.jsx
@@ -1,18 +1,19 @@
-'use client'
+'use client';
 
 import React from "react";
 import SideMenu from "../notification/_component/side-menu";
 import Notification from "../notification/_component/notificationFeed";
 import Nav from "../notification/_component/Nav";
 import { CldImage } from 'next-cloudinary';
-export default function page() {
+
+export default function Page() {
   return (
     <>
       <Nav />
       <div className="notification mt-4">
         <div>
           <CldImage
-            src="https://res.cloudinary.com/dn4hkronr/image/upload/v1735045652/bg-img3_sjyfvr.png"
+            src="bg-img3_sjyfvr"
             alt="background image"
             width={200}
             height={200}
@@ -22,7 +23,6 @@ export default function page() {
         <div className="notification-content px-4 py-2 w-90 lg:w-96 sm:left-6 relative rounded-t-lg shadow-custom bg-white left-8 -mt-11">
           <div className="mt-14 flex">
             <SideMenu />
-            <div className=""/>
             <Notification />
           </div>
         </div>
