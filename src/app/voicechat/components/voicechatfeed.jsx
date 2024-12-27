c35dcced34c958ac57a9ecb2f539cc15894fbf13
Woruka Onward Onyekachi <worukaonward@gmail.com>
2024-12-27 16:09:18 +0100

    working on the new voice chat ui

---
+++ src/app/voicechat/components/voicechatfeed.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function voicechatfeed() {
+  return (
+    <div className="absolute top-28 z-10 bg-white w-93 sm:w-74 shadow-lg rounded left-4 sm:left-7">
+      <div className='w-4/5 mx-auto'>
+        <h1 className="bg-blue-500 w-40 mx-auto rounded-2xl mt-4 p-1 text-white text-lg  text-center">
+          VECHTRON UI
+        </h1>
+        <p className="mt-10 text-center text-xl sm:text-2xl font-semibold">
+          Good day! How may I assist you today?
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/app/voicechat/page.jsx
+++ src/app/voicechat/page.jsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 import React from "react";
 import SideChat from "../voicechat/components/sideChat";
 import "@app/chat/chat.css";
-import Feed from "@components/chatComp/feed";
+import VoiceChatFeed from "./components/voicechatfeed";
 import Navbar from "@components/navbar/chatNav";
-import './voicechat.css'
+import "./voicechat.css";
 
 export default function page() {
   return (
@@ -18,31 +18,11 @@ export default function page() {
               alt="background-image"
               width={200}
               height={200}
-              className="backgroud-image sm:w-full lg:w-78 absolute h-32 cover-fill"
+              className="backgroud-image w-full lg:w-78 absolute h-32 cover-fill"
             />
-            <div className="voice-chat-content">
-              <div className=" text-black-50 absolute lg:w-74 w-96 lg:mx-7 mx-10 my-24 px-2 py-3 rounded-md bg-white">
-                <div className="voice-chat-text items-center flex gap-2 cursor-pointer">
-                  <Image
-                    src="/assets/icons/vechtron.png"
-                    alt="background-image"
-                    width={20}
-                    height={20}
-                  />
-                  <div className="text-md">Vechtron</div>
-                  <Image
-                    src="/assets/icons/dropdown.png"
-                    alt="background-image"
-                    width={5}
-                    height={5}
-                    className="w-2 h-3"
-                  />
-                </div>
-              </div>
-            </div>
-              <Feed />
+            <VoiceChatFeed />
           </div>
-          <div className="voice-chat-right sm:hidden md:block lg:block px-6">
+          <div className="hidden sm:flex px-6">
             <SideChat />
           </div>
         </div>
