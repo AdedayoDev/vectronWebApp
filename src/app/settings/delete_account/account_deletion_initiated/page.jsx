dfcaf7b910b33d70f199afa5155c3bd6d0d354d3
Woruka Onward Onyekachi <worukaonward@gmail.com>
2025-01-13 14:49:31 +0100

    Settings page completed

--- src/app/settings/delete_account/account_deletion_initiated/page.jsx
+++ src/app/settings/delete_account/account_deletion_initiated/page.jsx
@@ -1,31 +1,21 @@
 "use client";
 import { CldImage } from "next-cloudinary";
-import SettingsSideBar from "../../settings/components/SettingsSideBar";
-import { Check, Lock, LucideBatteryWarning, Trash } from "lucide-react";
+import SettingsSideBar from "../../components/SettingsSideBar";
 import { useState } from "react";
+import { FaTimes } from "react-icons/fa";
+import { useRouter } from "next/navigation";
 
 export default function Delete_Account() {
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const [alert, setAlert] = useState("");
-  const [secondAlert, setSecondAlert] = useState("");
 
-  function handleAlert() {
-    setSecondAlert([]);
-    setAlert('');
+  const [deleted, setDeleted] = useState([]);
+  const router = useRouter();
+
+  function handleDeleted() {
+    setTimeout(() => {
+      setDeleted("");
+    }, 1000);
+    router.push("/settings");
   }
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (password.length >= 12) {
-      setAlert([]);
-    } else {
-      setError("Password must be at least 12 characters long.");
-      setTimeout(() => {
-        setError("");
-      }, 3000);
-    }
-  };
 
   return (
     <>
@@ -39,132 +29,31 @@ export default function Delete_Account() {
             className="w-[95%] h-[50px] object-cover mt-11 mx-auto"
           />
         )}
-        <div className="block md:block lg:flex gap-[100px] w-[90%] relative -top-5 px-4 pt-11 bg-white rounded-sm shadow mx-auto">
+        <div className="block md:block lg:flex gap-[100px] w-[90%] relative -top-5 px-4 pt-11 shadow mx-auto">
           <SettingsSideBar />
           <div className="w-full lg:mt-[65px] h-[560px] lg:h-[630px]">
-            <h1 className="font-bold text-2xl mb-20">
-              We're sorry to see you go
-            </h1>
-            <div className="lg:w-[80%]">
-              <p className="mb-11">
-                Warning: Deleting your account will permanently remove all of
-                your data and cannot be undone. This includes your profile,
-                conversations, comments, and any other information associated
-                with your account. Are you sure you want to proceed with
-                deleting your account?
-              </p>
-              <div>
-                <label className="text-gray-500">Your password</label>
-                <form className="mt-2" onSubmit={handleSubmit}>
-                  <div className="w-full border bg-white flex items-center gap-2 p-3 rounded-lg">
-                    <Lock size={20} />
-                    <input
-                      type="password"
-                      value={password}
-                      className="w-full outline-none"
-                      onChange={(e) => setPassword(e.target.value)}
-                    />
-                  </div>
-                  <p className="mt-2 text-gray-600 text-sm">
-                    Minimum of 12 characters
+            {/* Account deleted */}
+           <div className="fixed inset-0 bg-black bg-opacity-50 z-50">
+           {deleted && (
+              <div className="absolute w-[90%] lg:w-[40%] left-[30px] lg:left-[35%] z-40 shadow-2xl items-center lg:top-[30%] top-[35%] mx-auto py-14 px-10 bg-white-500 bg-gray-200 rounded-md">
+                <div className="mb-4 text-center">
+                  <h1 className="text-red-700 text-xl lg:text-2xl font-semibold  mb-4">
+                    Account Deletion Initiated
+                  </h1>
+                  <p>
+                    We are processing your request. Your account will be deleted
+                    within 30 days . If you change your mind you can restore it
+                    during this period by logging in.
                   </p>
-                  {error && (
-                    <p className="text-red-500 text-sm mt-2">{error}</p>
-                  )}
-                  <button
-                    type="submit"
-                    className="bg-red-600 flex items-center gap-3 text-white rounded-full mt-7 justify-center w-[250px] p-3"
-                  >
-                    Delete my account
-                    <Trash color="white" />
-                  </button>
-                </form>
+                </div>
+
+                <div className="absolute cursor-pointer right-4 top-5 p-1 rounded border border-red-600" onClick={handleDeleted}>
+                  <FaTimes color="red" />
+                </div>
+                {deleted}
               </div>
-
-              {/* Delete account confirmation alert */}
-              {alert && (
-                <div className="absolute w-[80%] lg:w-[45%] left-[50px] lg:left-[26%] z-40 shadow-lg items-center top-[20%]  mx-auto py-6 px-10 bg-white-500 bg-white rounded-md">
-                  <div className="mb-4">
-                    <h1 className="font-bold text-lg mb-4">
-                      Delete Account Confirmation
-                    </h1>
-                    <p className="text-gray-500 text-base mb-4">
-                      Are you sure you want to delete your account?
-                    </p>
-                    <p className="mb-1 text-gray-500">
-                      We are sorry to see you go. Deleting your account is
-                      permanent and will:
-                    </p>
-                    <ul className="text-gray-500 list-disc">
-                      <li className="mb-1 ml-7">Remove your personal data</li>
-                      <li className="mb-1 ml-7">
-                        Cancel any active subscription
-                      </li>
-                      <li className="mb-1 ml-7">
-                        Delete your profile and saved preferences
-                      </li>
-                      <li className="mb-1 ml-7">Erase your account history</li>
-                    </ul>
-                    <div className="flex items-center gap-2 mt-3 mb-5">
-                      <LucideBatteryWarning color="red" />
-                      <p className="text-sm font-bold">
-                        This action cannot be undone. All data will be
-                        permanently deleted within 30 days.
-                      </p>
-                    </div>
-                  </div>
-
-                  <div className="mb-3 flex items-center gap-5">
-                    <button
-                      onClick={handleAlert}
-                      className="bg-red-600 text-white rounded-full p-3 w-[200px]"
-                    >
-                      I understand
-                    </button>
-                    <button
-                      className="-white rounded-full p-2 w-[150px] border-4 border-solid border-purple-400"
-                      onClick={() => {
-                        setAlert("");
-                      }}
-                    >
-                      Cancel
-                    </button>
-                  </div>
-
-                  {alert}
-                </div>
-              )}
-
-              {/* Alert2 */}
-              {secondAlert && (
-                <div className="absolute w-[80%] lg:w-[30%] left-[50px] lg:left-[33%] z-40 shadow-lg items-center lg:top-[20%] top-[30%] mx-auto py-6 px-10 bg-white-500 bg-white rounded-md">
-                  <div className="mb-4">
-                    <p className="text-gray-500 text-base mb-4">
-                      Are you sure you want to delete your account?
-                    </p>
-
-                    <ul className="text-gray-500 list-disc">
-                      <li className="mb-1 ml-7">Remove your personal data</li>
-                      <li className="mb-1 ml-7">
-                        Cancel any active subscription
-                      </li>
-                      <li className="mb-1 ml-7">
-                        Delete your profile and saved preferences
-                      </li>
-                      <li className="mb-1 ml-7">Erase your account history</li>
-                    </ul>
-                  </div>
-
-                  <div className="mb-3">
-                    <button className="bg-blue-600 text-white rounded-full p-3">
-                      Submit
-                    </button>
-                  </div>
-
-                  {secondAlert}
-                </div>
-              )}
-            </div>
+            )}
+           </div>
           </div>
         </div>
       </section>
--- src/app/settings/delete_account/page.jsx
+++ src/app/settings/delete_account/page.jsx
@@ -1,10 +1,13 @@
 "use client";
 import { CldImage } from "next-cloudinary";
 import SettingsSideBar from "../../settings/components/SettingsSideBar";
-import { Check, Lock, LucideBatteryWarning, Trash } from "lucide-react";
+import { Lock, LucideBatteryWarning, Trash } from "lucide-react";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
+import Image from 'next/image'
 
 export default function Delete_Account() {
+  const router = useRouter();
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const [alert, setAlert] = useState("");
@@ -13,13 +16,14 @@ export default function Delete_Account() {
 
   function handleAlert() {
     setSecondAlert([]);
-    setAlert('');
+    setAlert("");
   }
   function handleDelete() {
-    setSecondAlert('');
-    setTimeout(()=>{
-      setSubmit('')
-    },)
+    setSecondAlert("");
+    setTimeout(() => {
+      setSubmit("");
+    }, 3000);
+    router.push("/settings/delete_account/account_deletion_initiated");
   }
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -89,86 +93,104 @@ export default function Delete_Account() {
               </div>
 
               {/* Delete account confirmation alert */}
+
               {alert && (
-                <div className="absolute w-[80%] lg:w-[45%] left-[50px] lg:left-[26%] z-40 shadow-lg items-center top-[20%]  mx-auto py-6 px-10 bg-white-500 bg-white rounded-md">
-                  <div className="mb-4">
-                    <h1 className="font-bold text-lg mb-4">
-                      Delete Account Confirmation
-                    </h1>
-                    <p className="text-gray-500 text-base mb-4">
-                      Are you sure you want to delete your account?
-                    </p>
-                    <p className="mb-1 text-gray-500">
-                      We are sorry to see you go. Deleting your account is
-                      permanent and will:
-                    </p>
-                    <ul className="text-gray-500 list-disc">
-                      <li className="mb-1 ml-7">Remove your personal data</li>
-                      <li className="mb-1 ml-7">
-                        Cancel any active subscription
-                      </li>
-                      <li className="mb-1 ml-7">
-                        Delete your profile and saved preferences
-                      </li>
-                      <li className="mb-1 ml-7">Erase your account history</li>
-                    </ul>
-                    <div className="flex items-center gap-2 mt-3 mb-5">
-                      <LucideBatteryWarning color="red" />
-                      <p className="text-sm font-bold">
-                        This action cannot be undone. All data will be
-                        permanently deleted within 30 days.
+                <div className="fixed inset-0 bg-black bg-opacity-50 z-50">
+                  <div className="absolute w-[80%] lg:w-[45%] left-[60px] lg:left-[30%] z-40 shadow-lg items-center top-[25%]  mx-auto py-6 px-10 bg-white-500 bg-white rounded-md">
+                    <div className="mb-4">
+                      <h1 className="font-bold text-lg mb-4">
+                        Delete Account Confirmation
+                      </h1>
+                      <p className="text-gray-500 text-base mb-4">
+                        Are you sure you want to delete your account?
                       </p>
+                      <p className="mb-1 text-gray-500">
+                        We are sorry to see you go. Deleting your account is
+                        permanent and will:
+                      </p>
+                      <ul className="text-gray-500 list-disc">
+                        <li className="mb-1 ml-7">Remove your personal data</li>
+                        <li className="mb-1 ml-7">
+                          Cancel any active subscription
+                        </li>
+                        <li className="mb-1 ml-7">
+                          Delete your profile and saved preferences
+                        </li>
+                        <li className="mb-1 ml-7">
+                          Erase your account history
+                        </li>
+                      </ul>
+                      <div className="flex items-center gap-2 mt-3 mb-5">
+                        <Image
+                        src='/assets/icons/danger.svg'
+                        width={20}
+                        height={20}
+                        alt='danger'
+                        />
+                        <p className="text-sm font-bold">
+                          This action cannot be undone. All data will be
+                          permanently deleted within 30 days.
+                        </p>
+                      </div>
                     </div>
-                  </div>
 
-                  <div className="mb-3 flex items-center gap-5">
-                    <button
-                      onClick={handleAlert}
-                      className="bg-red-600 text-white rounded-full p-3 w-[200px]"
-                    >
-                      I understand
-                    </button>
-                    <button
-                      className="-white rounded-full p-2 w-[150px] border-4 border-solid border-purple-400"
-                      onClick={() => {
-                        setAlert("");
-                      }}
-                    >
-                      Cancel
-                    </button>
-                  </div>
+                    <div className="mb-3 flex items-center gap-5">
+                      <button
+                        onClick={handleAlert}
+                        className="bg-red-600 text-white rounded-full p-3 w-[200px]"
+                      >
+                        I understand
+                      </button>
+                      <button
+                        className="bg-white rounded-full p-2 w-[150px] border-4 border-solid border-purple-400"
+                        onClick={() => {
+                          setAlert("");
+                        }}
+                      >
+                        Cancel
+                      </button>
+                    </div>
 
-                  {alert}
+                    {alert}
+                  </div>
                 </div>
               )}
 
               {/* Alert2 */}
+
               {secondAlert && (
-                <div className="absolute w-[80%] lg:w-[30%] left-[50px] lg:left-[33%] z-40 shadow-lg items-center lg:top-[20%] top-[30%] mx-auto py-6 px-10 bg-white-500 bg-white rounded-md">
-                  <div className="mb-4">
-                    <p className="text-gray-500 text-base mb-4">
-                      Are you sure you want to delete your account?
-                    </p>
+                <div className="fixed inset-0 bg-black bg-opacity-50 z-50">
+                  <div className="absolute w-[80%] lg:w-[30%] left-[60px] lg:left-[37%] z-40 shadow-lg items-center top-[30%] mx-auto py-6 px-10 bg-white-500 bg-white rounded-md">
+                    <div className="mb-4">
+                      <p className="text-gray-500 text-base mb-4">
+                        Are you sure you want to delete your account?
+                      </p>
 
-                    <ul className="text-gray-500 list-disc">
-                      <li className="mb-1 ml-7">Remove your personal data</li>
-                      <li className="mb-1 ml-7">
-                        Cancel any active subscription
-                      </li>
-                      <li className="mb-1 ml-7">
-                        Delete your profile and saved preferences
-                      </li>
-                      <li className="mb-1 ml-7">Erase your account history</li>
-                    </ul>
+                      <ul className="text-gray-500 list-disc">
+                        <li className="mb-1 ml-7">Remove your personal data</li>
+                        <li className="mb-1 ml-7">
+                          Cancel any active subscription
+                        </li>
+                        <li className="mb-1 ml-7">
+                          Delete your profile and saved preferences
+                        </li>
+                        <li className="mb-1 ml-7">
+                          Erase your account history
+                        </li>
+                      </ul>
+                    </div>
+
+                    <div className="mb-3">
+                      <button
+                        className="bg-blue-600 text-white rounded-full p-3"
+                        onClick={handleDelete}
+                      >
+                        Submit
+                      </button>
+                    </div>
+
+                    {secondAlert}
                   </div>
-
-                  <div className="mb-3">
-                    <button className="bg-blue-600 text-white rounded-full p-3">
-                      Submit
-                    </button>
-                  </div>
-
-                  {secondAlert}
                 </div>
               )}
             </div>
